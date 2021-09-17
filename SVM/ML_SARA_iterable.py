3a130521e846b742607d0ec4feb273f23d4a7d66
Katie-Sosnowski <katie.oceangirl@gmail.com>
2021-09-17 09:54:03 -0700

    Update SARA data and scripts

--- SVM/ML_SARA_iterable.py
+++ SVM/ML_SARA_iterable.py
@@ -14,9 +14,7 @@ from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split, ShuffleSplit, cross_val_score
 from sklearn import svm
-import pandas as pd
-import sys
-import os
+from sklearn.model_selection import GridSearchCV
 
 #Matplotlib
 import matplotlib
@@ -25,36 +23,47 @@ from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg #, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
 
-#CSV (for writing data to file)
+#Data reading/writing
 import csv
+import pandas as pd
+import sys
+import os
 
 # Read in the data:
-os.chdir('/Users/jtsosnowski/Desktop/UA/YoonLab/CrudeOil/KATIE/Nov2020Deadline/SARA')
-data = pd.read_csv('OTA2_SARA_double_sliced.csv')
+os.chdir('/Users/jtsosnowski/Desktop/UA/YoonLab/CrudeOil/KATIE/SARA_new')
+data = pd.read_csv('SARA_train_rep1.csv')
 # Define y (actual class) and corresponding x (spectral data)
 y = data.loc[:,'levelSaturate'].values
-x = data.drop(['ID', 'Name', 'OldAlgorithmClassification', '%Saturate', 'levelSaturate', '%Aromatic', 'levelAromatic', '%Resin', 'levelResin', '%Asphaltene', 'levelAsphaltene'],axis=1)
-#Split into test vs train data (70% train, 30% test)
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.30, random_state=None)
-#Define PCA parameters
-PCA = PCA(n_components=10, svd_solver='randomized', whiten=True)
-#Fit training data
-X_train = PCA.fit_transform(x_train)
-#Run PCA on test data
-X_test = PCA.transform(x_test)
+x = data.drop(['ID', 'Name', '%Saturate', 'levelSaturate', '%Aromatic', 'levelAromatic', '%Resin', 'levelResin', '%Asphaltene', 'levelAsphaltene'],axis=1)
+# Run random splitting, PCA, and all SVM parameter combinations 100 times
+for i in range(10):
+    # Split into test vs train data (70% train, 30% test)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.30, random_state=None)
+    # Define PCA parameters
+    pca = PCA(n_components=8, svd_solver='randomized', whiten=True)
+    # Fit training data
+    X_train = pca.fit_transform(x_train)
+    # Run PCA on test data
+    X_test = pca.transform(x_test)
+    # Iterate through SVM models
+    highest_accuracy = 0
+    highest_accuracy_list = [['ex','ex','ex',0]]
+    for gam in (0.001, 0.01, 0.1, 1, 10, 100, 1000):
+        for c in (0.001, 0.01, 0.1, 1, 10, 100, 1000):
+            for ker in ('poly', 'linear', 'rbf'):
+                #Define SVM parameters
+                clf = svm.SVC(kernel=ker, gamma=gam, C=c)
+                # Run the classifier for ALL PCs to predict the test data
+                y_pred = clf.fit(X_train, y_train).predict(X_test)
+                accuracy = accuracy_score(y_test, y_pred)
+                # Keep a record of the accuracies - this will eliminate
+                # some but not all lower accuracies from ever being appended
+                if accuracy >= highest_accuracy:
+                    highest_accuracy_list.append([ker,gam,c,accuracy])
+                    # Reset highest accuracy each time a higher accuracy is found
+                    highest_accuracy = accuracy
+    # Only print the results that give the highest accuracy (could be tied between multiple)
+    print('Run %d:' %i, [acc for acc in highest_accuracy_list if acc[3]==highest_accuracy])
 
-#Iterate through SVM models
-highest_accuracy = [0,0,0,0]
-for gam in (0.001, 0.01, 0.1, 1, 10, 100, 1000):
-    for c in (0.001, 0.01, 0.1, 1, 10, 100, 1000):
-        for ker in ('poly', 'linear', 'rbf'):
-            #Define SVM parameters
-            clf = svm.SVC(kernel=ker, gamma=gam, C=c)
-            # Run the classifier for ALL PCs to predict the Test data
-            y_pred = clf.fit(X_train, y_train).predict(X_test)
-            accuracy = accuracy_score(y_test, y_pred)
-            if accuracy > highest_accuracy[3]:
-                highest_accuracy=[ker,gam,c,accuracy]
-print(highest_accuracy)
 
 
